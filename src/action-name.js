0d35f09b060f9187afddf93a0cc8705fbbb65f3e
Ashwani <tech.ashwani@gmail.com>
2019-08-13 12:12:18 +1000

    reverting some of the auto-fomatting done by vscode plugin, using lts name for node version

--- src/action-name.js
+++ src/action-name.js
@@ -1,10 +1,9 @@
 // @flow
 import startsWith from 'starts-with';
-import {
-    ACTION_PREFIX,
-    ACTION_SEPARATOR
-} from './constants';
+import { ACTION_PREFIX, ACTION_SEPARATOR } from './constants';
 
 const prefix = `${ACTION_PREFIX}${ACTION_SEPARATOR}`;
 
-export default (type: string): string => (startsWith(type, prefix) ? type : `${prefix}${type}`);
+export default (type: string): string => (
+    startsWith(type, prefix) ? type : `${prefix}${type}`
+);
